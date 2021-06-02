d8dba61114f67c9127f0963a961e2159df3eebec
Peter Conerly <pconerly@gmail.com>
2021-06-02 16:46:57 -0700

    print multiple instead of concat

--- setup_imgui_window.py
+++ setup_imgui_window.py
@@ -7,7 +7,7 @@ def impl_pysdl2_init():
     window_name = "Spectograph Viewer"
 
     if SDL_Init(SDL_INIT_EVERYTHING) < 0:
-        print("Error: SDL could not initialize! SDL Error: " + SDL_GetError())
+        print("Error: SDL could not initialize! SDL Error: ", SDL_GetError())
         exit(1)
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
@@ -32,19 +32,19 @@ def impl_pysdl2_init():
         SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI)
 
     if window is None:
-        print(
-            "Error: Window could not be created! SDL Error: " + SDL_GetError())
+        print("Error: Window could not be created! SDL Error: ",
+              SDL_GetError())
         exit(1)
 
     gl_context = SDL_GL_CreateContext(window)
     if gl_context is None:
-        print("Error: Cannot create OpenGL Context! SDL Error: " +
+        print("Error: Cannot create OpenGL Context! SDL Error: ",
               SDL_GetError())
         exit(1)
 
     SDL_GL_MakeCurrent(window, gl_context)
     if SDL_GL_SetSwapInterval(1) < 0:
-        print("Warning: Unable to set VSync! SDL Error: " + SDL_GetError())
+        print("Warning: Unable to set VSync! SDL Error: ", SDL_GetError())
         exit(1)
 
     return window, gl_context
